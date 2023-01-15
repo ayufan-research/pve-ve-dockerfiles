5478fefc08ed33c310a5c743c202caa2ae5c128d
Kamil Trzciński <ayufan@ayufan.eu>
2023-01-15 19:09:09 +0100

    Set tag

--- release.bash
+++ release.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-  echo "$0: usage <version>"
+if [[ $# -ne 1 ]] && [[ $# -ne 2 ]]; then
+  echo "$0: usage <version> [tag]"
   exit 1
 fi
 
 VERSION="$1"
+TAG="${2:-pve-ve-release}"
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR/"
 
@@ -14,4 +15,4 @@ docker build -f Dockerfile.env -t pve-ve-build-env "."
 docker build -f Dockerfile.release \
   --build-arg "VERSION=$VERSION" \
   --build-arg "IMAGE=pve-ve-build-env" \
-  -t pve-ve-release "."
+  -t "${TAG}" "."
