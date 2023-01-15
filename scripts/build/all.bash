814f0653edf8ed76096e781e6471b4ce4d899c8e
Kamil Trzciński <ayufan@ayufan.eu>
2023-01-15 18:51:25 +0100

    Fix all

--- scripts/build/all.bash
+++ scripts/build/all.bash
@@ -16,7 +16,7 @@ cd "/src/tmp/$VERSION"
 [[ -x /src/scripts/run/install.bash ]] && /src/scripts/run/install.bash "/src/tmp/$VERSION"
 /src/scripts/build/clone.bash "/src/versions/$VERSION/versions"
 /src/scripts/build/strip-cargo.bash
-/src/scripts/build/apply-patches.bash "/src/versions/$VERSION/pbs"
+/src/scripts/build/apply-patches.bash "/src/versions/$VERSION/pbs" || [[ -n "$FORCE" ]]
 /src/scripts/build/resolve-dependencies.bash
 /src/versions/"$VERSION"/build.bash
 /src/scripts/build/export-deb.bash "/src/deb/$VERSION"
