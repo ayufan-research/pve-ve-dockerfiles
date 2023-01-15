0a718891f2504dc2b0c4c862ed520e3e8d7f173f
Kamil Trzciński <ayufan@ayufan.eu>
2023-01-15 15:59:54 +0000

    Do not install backup server

--- scripts/export-deb.bash
+++ scripts/export-deb.bash
@@ -8,3 +8,5 @@ fi
 find "." -name '*.deb' | xargs -r -I{} cp {} "$1/"
 find "$1" -name '*-dev_*.deb' | xargs -r rm
 find "$1" -name '*-dbgsym_*.deb' | xargs -r rm
+find "$1" -name '*-dbg_*.deb' | xargs -r rm
+find "$1" -name 'proxmox-backup-server_*.deb' | xargs -r rm
